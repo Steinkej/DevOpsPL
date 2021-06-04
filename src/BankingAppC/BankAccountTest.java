d1f38bd205fd102b72a57107ed4fc57fe9657bc1
Jon <steinkej@th-brandenburg.de>
2021-06-04 15:56:06 +0200

    Initial commit

---
+++ src/BankingAppC/BankAccountTest.java
@@ -0,0 +1,38 @@
+package BankingAppC;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import BankingAppC.BankAccount;
+public class BankAccountTests {
+    @Test
+    public void testDeposit() {
+        BankAccount bankaccount = new BankAccount();
+        bankaccount.deposit(5000);
+        assertEquals(5000, bankaccount.getBalance());
+    }
+    @Test
+    public void testWithdraw(){
+        BankAccount bankaccount = new BankAccount();
+        bankaccount.deposit(5000);
+        bankaccount.withdraw(2000);
+        assertEquals(3000, bankaccount.getBalance());
+    }
+    @Test
+    public void testWithdrawforeignCurrency() {
+        BankAccount bankaccount = new BankAccount();
+    }
+
+    @Test
+    public void testWithdraw(){
+        BankAccount bankaccount = new BankAccount();
+        bankaccount.deposit(5000);
+        bankaccount.withdraw(2000);
+        assertEquals(3000, bankaccount.getBalance());
+    }
+    @Test
+    public void testWithdrawforeignCurrency(){
+        BankAccount bankaccount = new BankAccount();
+        bankaccount.deposit(5000);
+        bankaccount.withdraw(2000,2);
+        assertEquals(1000, bankaccount.getBalance());
+    }
+}
